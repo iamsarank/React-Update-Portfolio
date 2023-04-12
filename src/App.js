5f7b7b11245c8a8f5f92dab770486b6d6173f889
Madurai Corporation <itzsarank@gmail.com>
2023-04-12 18:23:12 +0530

    Components styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-
+import React from "react";
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import About from './components/about/About';
+import Experience from './components/experience/Experience';
+import Service from './components/service/Service';
+import Portfolio from './components/portfolio/Portfolio';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+ 
 function App() {
   return (
     <>
-      Welcome Back 
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Service />
+      <Portfolio />
+      <Contact />
+      <Footer />
     </>
   );
 }
